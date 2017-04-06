edd5f73bec444e37a28d5054da8b720e56fdc059
DESKTOP-UU3MDUP\ruiya <ruiyao_yin@163.com>
2017-04-06 11:07:03 +0800

    Basic functions completed

--- gallery-by-react/src/components/Main.js
+++ gallery-by-react/src/components/Main.js
@@ -90,6 +90,47 @@ class ImgFigure extends React.Component {
     }
 }
 
+class ControllerUnit extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick(e) {
+
+
+        //如果点击的是当前正在选中态的按钮，则翻转图片，否则将对应的图片居中
+        if (this.props.arrange.isCenter) {
+            this.props.inverse();
+        } else {
+            this.props.center();
+        }
+
+        e.stopPropagation();
+        e.preventDefault();
+    }
+
+    render() {
+
+        let controllerUnitClassName = "controller-unit";
+
+        //如果对应的是居中的图片，显示按钮的居中态
+        if (this.props.arrange.isCenter) {
+            controllerUnitClassName += " is-center";
+
+            //如果同时对应的时候翻转图片，显示按钮的翻转态
+            if (this.props.arrange.isInverse) {
+                controllerUnitClassName += " is-inverse";
+            }
+        }
+
+        return (
+            <span className={controllerUnitClassName} onClick={this.handleClick}></span>
+        );
+    }
+}
+
 class AppComponent extends React.Component {
 
     constructor() {
@@ -152,7 +193,7 @@ class AppComponent extends React.Component {
             vPosRangeX = vPosRange.x,
 
             imgsArrangeTopArr = [],
-            topImgNum = Math.ceil(Math.random() * 2), //取一个或者不取
+            topImgNum = Math.floor(Math.random() * 2), //取一个或者不取
             topImgSpliceIndex = 0,
             imgsArrangeCenterArr = imgsArrangeArr.splice(centerIndex, 1);
 
@@ -276,6 +317,9 @@ class AppComponent extends React.Component {
             imgFigures.push(<ImgFigure data={value} ref={'imgFigure' + index}
             arrange={this.state.imgsArrangeArr[index]} inverse={this.inverse(index)}
             center={this.center(index)}/>);
+
+            controllerUnits.push(<ControllerUnit arrange={this.state.imgsArrangeArr[index]}
+            inverse={this.inverse(index)} center={this.center(index)}/>);
         });
 
         return (
